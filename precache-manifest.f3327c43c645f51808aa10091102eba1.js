b6917f2735477e60b9b7fa5164909e68e7f2870d
m-onofri <manuele.onofri@gmail.com>
2019-12-29 22:11:53 +0100

    Updates

---
+++ precache-manifest.f3327c43c645f51808aa10091102eba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5872bee9472e5853ead297ed5a8f5c",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "172a46b38695fd08ecdf",
+    "url": "/drum-machine/static/css/main.4aef6ec0.chunk.css"
+  },
+  {
+    "revision": "b3d4d86f3d10a4e7c92c",
+    "url": "/drum-machine/static/js/2.81ac5291.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/drum-machine/static/js/2.81ac5291.chunk.js.LICENSE"
+  },
+  {
+    "revision": "172a46b38695fd08ecdf",
+    "url": "/drum-machine/static/js/main.82f6b2d1.chunk.js"
+  },
+  {
+    "revision": "31ab8682275b352d53f7",
+    "url": "/drum-machine/static/js/runtime-main.cc7fa7a2.js"
+  }
+]);
